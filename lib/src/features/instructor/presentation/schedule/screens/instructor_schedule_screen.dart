06cbf5fc139697276bf3db4e0fe36ffb933cc4b9
nawar <nawaraltibi46@gmail.com>
2026-08-07 21:32:15 +0300

    style(instructor): dart format for dark-mode CI

--- lib/src/features/instructor/presentation/schedule/screens/instructor_schedule_screen.dart
+++ lib/src/features/instructor/presentation/schedule/screens/instructor_schedule_screen.dart
@@ -36,8 +36,7 @@ class InstructorScheduleScreen extends StatelessWidget {
                       >(
                         buildWhen: (previous, current) =>
                             previous.apiState != current.apiState ||
-                            previous.isSilentRefresh !=
-                                current.isSilentRefresh,
+                            previous.isSilentRefresh != current.isSilentRefresh,
                         builder: (context, state) {
                           return state.apiState.when(
                             initial: () =>
@@ -52,8 +51,7 @@ class InstructorScheduleScreen extends StatelessWidget {
                                 child: Padding(
                                   padding: PaddingManager.paddingAll16,
                                   child: Column(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.center,
+                                    mainAxisAlignment: MainAxisAlignment.center,
                                     children: [
                                       Text(
                                         CoreFailureMessageMapper.messageFor(
